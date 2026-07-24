adbbf92c7f801820235f39db6823e4d9e75a8cc4
Mohammad Zaid Khan <zaidkhan24082006@gmail.com>
2026-07-24 23:36:52 +0530

    Domino

---
+++ domino.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int m,n;
+    cin>>m>>n;
+    
+    cout << (m*n)/2<<endl;
+    return 0;
+}
